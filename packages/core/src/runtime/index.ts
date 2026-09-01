2eb672368de7a52ab23f3dc346ff8ae2d464ae4f
gouchao <gouchao@bytedance.com>
2026-09-02 05:02:18 +0800

    chore: publish core v0.37.29

--- packages/core/src/runtime/index.ts
+++ packages/core/src/runtime/index.ts
@@ -9,7 +9,7 @@ import {
   normalizeRpcResponse,
 } from '../protocol/index.js'
 
-export const CODY_WEB_CORE_VERSION = '0.37.27'
+export const CODY_WEB_CORE_VERSION = '0.37.29'
 
 export type { RuntimeNotification, ServerRequest } from '../protocol/index.js'
 
